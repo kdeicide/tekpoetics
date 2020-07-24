4fd17ae2c030bb37441915773388c5203bdb7bd4
setimozac <sizdah@gmail.com>
2020-07-24 18:16:46 -0400

    modified key!

--- app/tekpoetics/settings.py
+++ app/tekpoetics/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-3u$es7j#-7&iiph3fhy_#(h!+%%5$0_#*1w@@f=hl%=@(%vc1'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
